f923e1a35c2d3ffabc62778870cc89b02a21840a
Bran-donC <100102594+Bran-donC@users.noreply.github.com>
2022-05-20 22:30:31 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 "use strict"
 //
 var frontCanvas = document.getElementById('rayCanvas');
-frontCanvas.width = document.body.clientWidth;
-frontCanvas.height = document.body.clientWidth;
+frontCanvas.width = document.body.clientWidth/2;
+frontCanvas.height = document.body.clientWidth/2;
 const frontctx = frontCanvas.getContext('2d', {alpha: false, desynchronized: true})
+
+function toDegrees(num){return num*Math.PI/180}
+function toReverseDegrees(num){return num*180/Math.PI}
 
 function main(){
     let incIndex = document.getElementById('incDex').value;
     let refIndex = document.getElementById('refDex').value;
     let incAngle = 90 - document.getElementById('incAngle').value; //Convert to degrees, and the numbers around work I guess
 
-    console.log('yess')
+    let refAngle = toReverseDegrees(Math.asin(((incIndex * Math.sin(toDegrees(incAngle)))/refIndex)))
 
-    let refAngle = Math.asin(((incIndex * Math.sin(incAngle*Math.PI / 180))/refIndex))*180/Math.PI
-    console.log(refAngle)
+    document.getElementById('refAngle').innerHTML = 'Reflective Angle '+Math.round(refAngle*100)/100 + '°'
 
     // Create temporary canvas
     let backCanvas = document.createElement('canvas');
@@ -30,25 +32,34 @@ function main(){
     // Paint normal
     ctx.lineWidth = backCanvas.width/400;
     ctx.fillRect(0, backCanvas.height/2, backCanvas.height, ctx.lineWidth);
+    ctx.fillRect(backCanvas.width/2-ctx.lineWidth/2, 0, ctx.lineWidth, backCanvas.height);
 
     //Paint incident ray, starting from the centre
     ctx.lineWidth = backCanvas.width/600;
-    ctx.strokeStyle = '#FF0000';
+    ctx.strokeStyle = '#00FF00';
     ctx.beginPath();
     ctx.moveTo(backCanvas.width/2,backCanvas.height/2);
     //ctx.moveTo(100,100)
     //Tan(angle) = opposite / adjacent
     //Math.tan(incAngle) = opposite / backCanvas.height/2
-    let opposite = Math.tan(incAngle*Math.PI / 180) * backCanvas.height/2
+    let opposite = Math.tan(toDegrees(incAngle)) * backCanvas.height/2
     ctx.lineTo(backCanvas.width/2-opposite,0);
 
     ctx.stroke();
 
-    //Paint reflective ray
+    //Paint reflectived ray
     ctx.beginPath();
+    ctx.strokeStyle = '#00F000'
     ctx.moveTo(backCanvas.width/2,backCanvas.height/2);
-    //ctx.moveTo(100,100)
-    opposite = Math.tan(refAngle*Math.PI / 180) * backCanvas.height/2
+    ctx.lineTo(backCanvas.width/2+opposite,0);
+
+    ctx.stroke();
+
+    //Paint refracted ray
+    ctx.beginPath();
+    ctx.strokeStyle = '#FF0000'
+    ctx.moveTo(backCanvas.width/2,backCanvas.height/2);
+    opposite = Math.tan(toDegrees(refAngle)) * backCanvas.height/2
     ctx.lineTo(backCanvas.width/2+opposite,backCanvas.height);
 
     ctx.stroke();
